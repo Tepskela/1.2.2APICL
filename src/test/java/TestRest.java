dca5295c24a43eecc0ce9dd740134bbd59059567
Aleksandr Petrenko <aleksandr_petrenko62@mail.ru>
2023-05-23 22:50:21 +0300

    Fail

--- src/test/java/TestRest.java
+++ src/test/java/TestRest.java
@@ -19,7 +19,7 @@ public class TestRest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Привет"))
+                .body("data", equalTo("Пока"))
         ;
     }
 }
